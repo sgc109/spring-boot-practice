74bb0f2cedcbaf1b3d529e62eb6bd56e375c11a4
sungho <shoo.hwang@samsung.com>
2020-03-10 22:08:41 +0900

    finish chapter 4

---
+++ src/test/java/com/example/SpringBootCommunityWeb/SpringBootCommunityWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.SpringBootCommunityWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+//@SpringBootTest
+class SpringBootCommunityWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
